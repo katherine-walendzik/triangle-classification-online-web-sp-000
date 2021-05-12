9c3967599292bfaf24618eb90057e2366737611f
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-12 17:38:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,42 @@ class Triangle
   end
   
   def kind
-    if @sides.
+    if self.invalid?
+        raise TriangleError
+    elsif self.equilateral?
+      :equilateral
+    elsif self.isosceles?
+      :isosceles
+    elsif self.scalene?
+      :scalene
+    end
   end
-  
+
+  def invalid?
+    if (self.sides.any?{ |side| side <= 0} ||
+      self.side1 + self.side2 <= side3 ||
+      self.side2 + self.side3 <= side1 ||
+      self.side3 + self.side1 <= side2)
+      true
+    end
+  end
+
+  def equilateral?
+    true if sides.uniq.count == 1
+  end
+
+  def isosceles?
+    true if sides.uniq.count == 2
+  end
+
+  def scalene?
+    true if sides.uniq.count == 3
+  end
+
   class TriangleError < StandardError
-    # triangle error code
+    def message
+      "This is not a valid triangle!"
+    end
   end
   
 end
